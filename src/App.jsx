1cacd21e464c8e99886e7f033668ecf1bcf4e529
ohhkaneda.eth <87439324+ohhkaneda@users.noreply.github.com>
2022-01-01 17:31:24 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -194,7 +194,7 @@ const App = () => {
   if (!address) {
     return (
       <div className="landing">
-        <h1>Welcome to NarutoDAO</h1>
+        <h1>Welcome to GMfoodDAO</h1>
         <button onClick={() => connectWallet("injected")} className="btn-hero">
           Connect your wallet
         </button>
